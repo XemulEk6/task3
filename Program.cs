6c17d3e5e903daa13b39d95bcb7c3302e2cabfd0
XemulEk6 <xemul.ek6@gmail.com>
2023-03-03 17:26:26 +0500

    endtask

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число:");
+int Nember = Convert.ToInt32(Console.ReadLine());
+
+
+            if (Nember % 2 == 1)
+            {
+                Console.WriteLine($"Число {Nember} является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число {Nember}является: ЧЁТНЫМ");
+            }
